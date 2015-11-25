d58dd55edf33b4d643ffd142f24ed03e8720ba21
jiangtd <jiangtd@jiangtddemacbook-air.local>
2015-11-25 09:12:32 +0800

    Initial Commit

---
+++ 饼状图Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  饼状图Demo
+//
+//  Created by jiangtd on 15/11/25.
+//  Copyright © 2015年 jiangtd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 饼状图Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  饼状图Demo
+//
+//  Created by jiangtd on 15/11/25.
+//  Copyright © 2015年 jiangtd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
